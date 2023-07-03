7fb86d70ee37c499b79e41a2f003bb3a88097388
juancgalueweb <juancgalue@icloud.com>
2023-07-02 21:58:08 -0400

    Agrega imágenes con efectos y logo principal

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        boxing: ['Boxing', 'sans-serif'],
+        'boxing-striped': ['Boxing Striped', 'sans-serif']
+      }
+    }
+  },
+  plugins: [require('tailwindcss-animated')]
 }
